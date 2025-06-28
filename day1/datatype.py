8c81932cc44dc1223f8d1b6d73af453decdde70e
avantikaaa01 <avantikabibhu@gmail.com>
2025-06-28 08:25:58 +0530

    Removed al the submodule and added day1 and day2 as normal folder

---
+++ day1/datatype.py
@@ -0,0 +1,52 @@
+a = 20
+b = 4.5
+c = "Hello, I am avantika"
+d = True
+
+print( "\n",a,"\n",b,"\n",c,"\n",d) # print all the output in next line.
+
+ab = {20, 67, 90, 44, 67}
+print(ab) # set is unordered but it does not accept duplicate values.
+
+ac = [23, 67, 8, 65, 3,8]
+print(ac) # list is ordered but it accepts duplicate values also it is mutuable.
+
+ad = (23, 56, 78, 32, 32, 56)
+print(ad) # tuple is ordered but itcaccept duplicate values also it is immutable.
+
+ae = {"name" :"Avantika", "age" : 20, "city": "delhi" }
+print(ae)
+print(ae.keys())
+print(ae.values())
+print(ae.items())
+
+# What is Type Conversion?
+
+ay = "100"
+by = 20
+print(int(ay) + by)
+
+# Convert the string "45" to an integer and add 10 to it.
+az = "45"
+print(int(az)+ 10)
+
+# Convert the float 9.8 to an integer and print the result.
+ax = 9.8
+print(int(ax) + 10)
+
+# Take the integer 100, convert it to a string, and print:
+
+au = 100
+aw = str(au)
+print(str(au))
+print(type(aw))
+
+
+
+
+
+
+
+
+
+
---
+++ day2/conditional_statement.py
@@ -0,0 +1,28 @@
+# Input a number and check if it is even or odd
+# a = int(input("Enter a number"))
+# if a % 2 ==0:
+#     print("It is even number")
+# else:
+#     print("It is odd number")
+
+
+# Hardcoded username/password check
+# username = input("enter username")
+# password = int(input("enter password"))
+# if(username == "admin" and password == 1234):
+#     print("login Successfull")
+# else:
+#     print("Invalid username and password")
+
+# Input 3 numbers and print the largest
+num1 = int(input("enter the num1: "))
+num2 = int(input("enter num2: "))
+num3 = int(input("enter num3: "))
+
+if(num1> num2 and num1 > num3):
+    print(num1, " is a greater")
+elif(num2> num1 and num2 > num3):
+    print(num2, " is a greater")
+else:
+    print(num3, " is greater")
+
